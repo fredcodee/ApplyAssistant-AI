caead0bddb6346ae4ee85d6c709ceb5e174f3b71
fredcode <fredcode23@gmail.com>
2024-09-20 20:41:07 +0800

    user auth initiated

--- server/controllers/AppController.js
+++ server/controllers/AppController.js
@@ -1,4 +1,7 @@
 const errorHandler = require("../middlewares/ErrorHandler")
+const User = require("../models/UserModel")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 
 const health = async (req, res) => {
@@ -6,7 +9,84 @@ const health = async (req, res) => {
 }
 
 
+//user auth
+const login = async (req, res) => {
+    try{
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(400).json({
+                title: 'bad request',
+                message: 'email and password are required'
+            })
+        }   
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(401).json({
+                title: 'unauthorized',
+                message: 'email or password is incorrect'
+            })
+        }
+        const isMatch = bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(401).json({
+                title: 'unauthorized',
+                message: 'email or password is incorrect'
+            })
+        }   
+
+        const token = jwt.sign({ id: user._id , email: user.email}, process.env.JWT_SECRET, { expiresIn: '1d' })
+        return res.cookie("access_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "Production",
+            }).status(200).json({ message: "Logged in successfully " });
+    }
+    catch(error){
+        return errorHandler(error, res)
+    }
+}
+
+
+const logout = async (req, res) => {
+    return res.clearCookie("access_token").status(200).json({ message: "Logged out successfully " });
+}
+
+
+const register = async (req, res) => {
+    try{
+        try {
+            const { email, password } = req.body
+
+            const registered = await User.findOne({ email })
+            if (registered) {
+                return res.status(401).json({ message: 'This email is already registered' });
+            }
+            const newUser = new User({
+                email,
+                password: bcrypt.hashSync(password, 10)
+            })
+            await newUser.save()
+            return res.json({ message: 'user is registered successfully' });
+        } catch (error) {
+            errorHandler.errorHandler(error, res)
+        }
+    }
+    catch(error){
+        return errorHandler(error, res)
+    }
+}
+
+
+const userDetails = async (req, res) => {
+    try{
+        return res.status(200).json({
+            id: req.userId, email: req.userEmail});
+    }
+    catch(error){
+        return errorHandler(error, res)
+    }
+}
+
 module.exports = {
-    health
+    health,login,logout,register,userDetails
 
 }
