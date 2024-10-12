e3deebe4424aaef623c25e8f47df1e649ea226b5
fredcodee <fredcode23@gmail.com>
2024-10-12 20:22:48 +0800

    frontend- accept uploads of user resume

--- client/aiapplyassistant/src/pages/Dashboard.jsx
+++ client/aiapplyassistant/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button"
 import Header from '@/components/Header'
 import Api from '../Api';
 import { Input } from "@/components/ui/input"
+import { Loader2 } from "lucide-react"
+import { Label } from '@/components/ui/label';
 
 
 const Dashboard = () => {
@@ -10,6 +12,7 @@ const Dashboard = () => {
   const [error, setError] = useState(null)
   const [success, setSuccess] = useState('')
   const [resume, setResume] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const checkUserResume = async () => {
     try {
@@ -36,6 +39,34 @@ const Dashboard = () => {
     }
   }
 
+  const uploadResume = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append('pdf', file);
+      await Api.post('api/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token.replace(/"/g, '')}`
+        }
+      })
+        .then((response) => {
+          if (response.status == 200) {
+            setSuccess("Resume uploaded successfully, you can now post jobs details and get started :)")
+            setLoading(false)
+          }
+        })
+    }
+    catch (error) {
+      setError("Oops, something went wrong. Please try again later.")
+      setLoading(false)
+    }
+  }
+
+  const handleResumeUpload = (event) => {
+    setLoading(true)
+    uploadResume(event.target.files[0])
+  }
+
   useEffect(() => {
     checkUserResume()
   }, [])
@@ -49,25 +80,48 @@ const Dashboard = () => {
             <span>{error}</span>
           </div>
         )}
+
+        {success && (
+          <div role="alert" className="alert alert-success max-w-sm mx-auto">
+            <span>{success}</span>
+          </div>
+        )}
       </div>
 
-      {resume && (
+      {resume && !loading && (
         <div className='container pt-4 text-center' style={{ margin: 'auto' }}>
           <h3 className='text-2xl'>Your Resume</h3>
         </div>
       )}
 
-      {!resume && (
+      {!resume && !loading && (
         <div className='container pt-4 text-center' style={{ margin: 'auto' }} >
           <h3>You have no resume uploaded yet</h3>
           <p>Upload your resume to get started ...</p>
-          <div className='flex justify-center pt-3'>
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Input id="picture" type="file" />
+
+          {!loading && (
+            <div className='flex justify-center pt-3'>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Input id="picture" type="file"
+                  accept=".pdf"
+                  onChange={handleResumeUpload}
+                />
+              </div>
             </div>
-          </div>
+          )}
         </div>
       )}
+       {loading && (
+        <div className='text-center pt-3'>
+          <p>AI assistant is processing your resume</p>
+          <div className='flex justify-center pt-3'>
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            </div>
+        </div>
+          )}
     </div>
   )
 }
